c5cc42a3bda3e73ea5b66b5de2e7a268107ab09c
sarahb831 <smbeauchamp885@gmail.com>
2021-05-03 17:19:15 -0700

    update nav style and behavior

---
+++ code/js/index.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link'); // do for all links
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+// for each nav-link, if there is a click on one, remove the nav-open class
+navLinks.forEach(link => { 
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
